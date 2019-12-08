e299470c763ebf01e3d5660a172733c8a2ba0b50
ixor911 <igorbutenko9111@gmaul.com>
2019-12-08 18:23:15 +0100

    3_fib_size_color

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "including.hpp"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 int main(){
     cout<<"<table border=\"1\" cellpadding=\"15\">";
     cout<<"<tr><td>M</td><td>L</td></tr>";
+    srand(time(NULL));
         for(int m=2;m<=9;m++)
         {
             int a=1,b=0,x=1,y=0,i=2;
@@ -15,11 +18,11 @@ int main(){
             x=fib_del(a,m);
             y=fib_del(b,m);
             cout<<"<tr><td>"<<m<<"</td><td>"<<length(a,b,x,y,i,m)<<"</td>";
-            cout<<"<td>"<<chisla(a,b,x,y,m,_0,0)<<"</td><td>"<<chisla(a,b,x,y,m,_1,1)<<"</td>";
-            cout<<"<td>"<<chisla(a,b,x,y,m,_2,2)<<"</td><td>"<<chisla(a,b,x,y,m,_3,3)<<"</td>";
-            cout<<"<td>"<<chisla(a,b,x,y,m,_4,4)<<"</td><td>"<<chisla(a,b,x,y,m,_5,5)<<"</td>";
-            cout<<"<td>"<<chisla(a,b,x,y,m,_6,6)<<"</td><td>"<<chisla(a,b,x,y,m,_7,7)<<"</td>";
-            cout<<"<td>"<<chisla(a,b,x,y,m,_8,8)<<"</td><td>"<<chisla(a,b,x,y,m,_9,9)<<"</td>";
+            cout<<"<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_0,0)<<"</td><td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_1,1)<<"</td>";
+            cout<<"<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_2,2)<<"</td><<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_3,3)<<"</td>";
+            cout<<"<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_4,4)<<"</td><<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_5,5)<<"</td>";
+            cout<<"<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_6,6)<<"</td><<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_7,7)<<"</td>";
+            cout<<"<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_8,8)<<"</td><<td bgcolor=\"rgb("<<rand()%254+1<<","<<rand()%254+1<<","<<rand()%254+1<<")\">"<<chisla(a,b,x,y,m,_9,9)<<"</td>";
         }
     cout<<"</table>";
 
